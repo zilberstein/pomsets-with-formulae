0bd263ade8bf5262067437ea52fc09309007ee40
Noam Zilberstein <noamzilberstein@me.com>
2026-07-22 14:45:07 -0400

    remove DCPO file

--- Pom/Order/DCPO.lean
+++
@@ -1,6 +0,0 @@
-import Pom.Order.FinApprox
-
-instance {l : Type} [DCPO l] [ScottCompact l] [OrderBot l] : ChainCompletePartialOrder (Pom l) where
-  cSup := sorry
-  le_cSup := sorry
-  cSup_le := sorry
